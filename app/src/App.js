6b16e476a0735f007428c3e1b691e24e4681a73b
cwen <2067316044@QQ.com>
2023-03-16 20:54:43 +0800

    25

--- app/src/App.js
+++ app/src/App.js
@@ -1,125 +1,62 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
-import { Table } from "antd";
-import { MinusCircleTwoTone, PlusCircleTwoTone } from "@ant-design/icons";
+import { Modal, Table, Upload } from "antd";
 
-const data = [
-	{
-		key: 1,
-		name: "John Brown sr.",
-		age: 60,
-		address: "New York No. 1 Lake Park",
-		children: [
-			{
-				key: 11,
-				name: "John Brown",
-				age: 42,
-				address: "New York No. 2 Lake Park",
-			},
-			{
-				key: 12,
-				name: "John Brown jr.",
-				age: 30,
-				address: "New York No. 3 Lake Park",
-				children: [
-					{
-						key: 121,
-						name: "Jimmy Brown",
-						age: 16,
-						address: "New York No. 3 Lake Park",
-					},
-				],
-			},
-			{
-				key: 13,
-				name: "Jim Green sr.",
-				age: 72,
-				address: "London No. 1 Lake Park",
-				children: [
-					{
-						key: 131,
-						name: "Jim Green",
-						age: 42,
-						address: "London No. 2 Lake Park",
-						children: [
-							{
-								key: 1311,
-								name: "Jim Green jr.",
-								age: 25,
-								address: "London No. 3 Lake Park",
-							},
-							{
-								key: 1312,
-								name: "Jimmy Green sr.",
-								age: 18,
-								address: "London No. 4 Lake Park",
-							},
-						],
-					},
-				],
-			},
-		],
-	},
-	{
-		key: 2,
-		name: "Joe Black",
-		age: 32,
-		address: "Sydney No. 1 Lake Park",
-	},
-];
 const App = () => {
-	const columns = [
+	const [fileList, setFileList] = useState([
 		{
-			title: "Name",
-			dataIndex: "name",
-			key: "name",
+			uid: "-1",
+			name: "image.png",
+			status: "done",
+			url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
 		},
 		{
-			title: "Age",
-			dataIndex: "age",
-			key: "age",
-			width: "12%",
+			uid: "rc-upload-1678970780203-2",
+			lastModified: 1663239550461,
+			lastModifiedDate: "2022-09-15T10:59:10.461Z",
+			name: "0d6e8e683c0980e33dd5f46d43bca027.jpeg",
+			size: 86377,
+			type: "image/jpeg",
+			percent: 100,
+			originFileObj: {
+				uid: "rc-upload-1678970780203-2",
+			},
+			status: "done",
+			response: {
+				code: 0,
+				msg: "succeed",
+				result: "//img-dove.oss-cn-hongkong.aliyuncs.com/8w-study/5ea0902e6b1b403d7a9f9645edad34321678966351.tmp",
+			},
+			xhr: {},
+			thumbUrl: "/logo192.png",
 		},
-		{
-			title: "Address",
-			dataIndex: "address",
-			width: "30%",
-			key: "address",
-		},
-	];
-	return (
-		<div className="App">
-			<Table
-				columns={columns}
-				rowSelection={{}}
-				expandIconAsCell={false}
-				expandable={{
-					// expandedRowRender: record => (
-					// 	<p
-					// 		style={{
-					// 			margin: 0,
-					// 		}}>
-					// 		{record.description}
-					// 	</p>
-					// ),
-					expandIconAsCell: false,
-					childrenColumnName: "children",
-					expandRowByClick: true,
-					// expandIcon: ({ expanded, onExpand, record }) => {
-					// 	expanded ? (
-					// 		<MinusCircleTwoTone
-					// 			onClick={e => onExpand(record, e)}
-					// 		/>
-					// 	) : (
-					// 		<PlusCircleTwoTone
-					// 			onClick={e => onExpand(record, e)}
-					// 		/>
-					// 	);
-					// },
-				}}
-				dataSource={data}
-			/>
+	]);
+
+	const uploadButton = (
+		<div>
+			<div
+				style={{
+					marginTop: 8,
+				}}>
+				Upload
+			</div>
 		</div>
+	);
+	const handleChange = ({ fileList: newFileList }) => {
+		setFileList(newFileList);
+	};
+	return (
+		<>
+			<Upload
+				action="/Home/Apis/Index/_c/upload"
+				listType="picture-card"
+				fileList={fileList}
+				name="file"
+				onChange={handleChange}
+				data={{ type: "upload" }}>
+				{fileList.length >= 8 ? null : uploadButton}
+			</Upload>
+		</>
 	);
 };
 
