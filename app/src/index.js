06a0422c680c32d3e4d45d461c3ebf594ccd4065
cwen <2067316044@QQ.com>
2023-03-16 08:10:24 +0800

    25

--- app/src/index.js
+++ app/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { ConfigProvider } from "antd";
+import zhCN from "antd/locale/zh_CN";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+	<ConfigProvider locale={zhCN}>
+		<App />
+	</ConfigProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
